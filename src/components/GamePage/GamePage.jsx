83b81a28258a2d3838f28bfbe3b25c9acd578568
mrglk <mariagalko.info@gmail.com>
2022-05-22 00:51:38 +0200

    Added buttons for card

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -3,52 +3,45 @@ import Card from "../Card/Card";
 // import Words from "../../words.json";
 import { useState } from "react";
 
-export default function GamePage({ id = 1, english = "Error", transcription = "|ˈerər|", russian = "Ошибка", chosenCard = 0, words = [...words] }) {
+export default function GamePage(props) {
+    const Words = props.words;
     const [checked, setCheked] = useState(false);
     const handleClick = () => {
         setCheked(!checked);
     }
 
-    const [index, setIndex] = useState(chosenCard);
+    const [index, setIndex] = useState(0);
 
-    const handleButtons = (direction) => {
-        let newIndex = index;
-        switch (direction) {
-            case 'next':
-                ++newIndex;
-                break;
-            case 'back':
-                --newIndex;
-                break;
-            default:
-                ++newIndex;
-        }
-
-        if (newIndex < 0) {
-            newIndex = words.length - 1;
-        } else if (newIndex >= words.length) {
-            newIndex = 0;
-        }
-
+    const handleButtons = (step = 1) => {
+        const newIndex = checkIndex(index + step);
         setIndex(newIndex);
         setCheked(false);
+    }
+
+    const checkIndex = (index) => {
+        if (index < 0) {
+            return index = Words.length - 1;
+        } else if (index >= Words.length) {
+            return index = 0;
+        }
+        return index;
     }
 
     return (
         <div className={styles.Wrapper}>
             <div className={styles.Slider}>
-                <button className={styles.Buttons} onClick={() => handleButtons("back")}> &lt; </button>
+                <button className={styles.Buttons} onClick={() => handleButtons(-1)}> <span>&lt; </span></button>
                 <Card
-                    key={words[index].id}
-                    english={words[index].english}
-                    transcription={words[index].transcription}
-                    russian={words[index].russian}
+                    key={Words[index].id}
+                    english={Words[index].english}
+                    transcription={Words[index].transcription}
+                    russian={Words[index].russian}
                     checked={checked}
                     onClick={handleClick} />
 
-                <button className={styles.Buttons} onClick={() => handleButtons("next")}> &gt; </button>
+                <button className={styles.Buttons} onClick={() => handleButtons(1)}> &gt; </button>
             </div>
-            <div className={styles.CountWrapper}><span className={styles.Count}>{index + 1}</span> / <span className={styles.Count}>{words.length}</span></div>
+            <div className={styles.CountWrapper}><span className={styles.Count}>{index + 1}</span> / <span className={styles.Count}>{Words.length}</span></div>
         </div>
     )
 }

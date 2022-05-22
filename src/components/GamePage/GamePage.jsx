6e307cf1193cf72c92a764970f99005219a398f3
mrglk <mariagalko.info@gmail.com>
2022-05-22 21:42:28 +0200

    Added check for words

--- src/components/GamePage/GamePage.jsx
+++ src/components/GamePage/GamePage.jsx
@@ -5,18 +5,13 @@ import { useState } from "react";
 
 export default function GamePage(props) {
     const Words = props.words;
+
     const [checked, setCheked] = useState(false);
     const handleClick = () => {
         setCheked(!checked);
     }
 
     const [index, setIndex] = useState(0);
-
-    const handleButtons = (step = 1) => {
-        const newIndex = checkIndex(index + step);
-        setIndex(newIndex);
-        setCheked(false);
-    }
 
     const checkIndex = (index) => {
         if (index < 0) {
@@ -27,21 +22,32 @@ export default function GamePage(props) {
         return index;
     }
 
-    return (
-        <div className={styles.Wrapper}>
-            <div className={styles.Slider}>
-                <button className={styles.Buttons} onClick={() => handleButtons(-1)}> <span>&lt; </span></button>
-                <Card
-                    key={Words[index].id}
-                    english={Words[index].english}
-                    transcription={Words[index].transcription}
-                    russian={Words[index].russian}
-                    checked={checked}
-                    onClick={handleClick} />
+    const handleButtons = (step = 1) => {
+        const newIndex = checkIndex(index + step);
+        setIndex(newIndex);
+        setCheked(false);
+    }
 
-                <button className={styles.Buttons} onClick={() => handleButtons(1)}> &gt; </button>
+    if (Words) {
+        return (
+            <div className={styles.Wrapper}>
+                <div className={styles.Slider}>
+                    <button className={styles.Buttons} onClick={() => handleButtons(-1)}> <span>&lt; </span></button>
+                    <Card
+                        key={Words[index].id}
+                        english={Words[index].english}
+                        transcription={Words[index].transcription}
+                        russian={Words[index].russian}
+                        checked={checked}
+                        onClick={handleClick} />
+                    <button className={styles.Buttons} onClick={() => handleButtons(1)}> &gt; </button>
+                </div>
+                <div className={styles.CountWrapper}><span className={styles.Count}>{index + 1}</span> / <span className={styles.Count}>{Words.length}</span></div>
             </div>
-            <div className={styles.CountWrapper}><span className={styles.Count}>{index + 1}</span> / <span className={styles.Count}>{Words.length}</span></div>
-        </div>
-    )
+        )
+    }
+
+    return (<div className={styles.Wrapper_error}>
+        Нет данных :(
+    </div>)
 }
